ae0c136f6b9689d498416de17afe22a66f0b00a1
Tejas Aghade <83055140+TejasAghade@users.noreply.github.com>
2024-05-20 12:20:20 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ function App() {
         <HomeThird/>
         <HomeFour/>
       </div>
-      
       <div className="wp-icon">
         <img src="assets/wp.png" alt="not found" />
         <span>WhatsApp</span>
